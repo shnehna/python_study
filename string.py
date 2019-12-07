055b6fdff8fede0d54ffd326e5f63779ca0a602e
shnehna <mr_shenhan@outlook.com>
2019-12-07 12:59:07 +0800

    str

---
+++ string.py
@@ -0,0 +1,9 @@
+num_str = "Hello world "
+# print(num_str.isdecimal())
+# print(num_str.isdigit())
+# print(num_str.isnumeric())
+print(num_str.startswith("H"))
+print(num_str.endswith("d"))
+print(num_str.find(" "))
+print(num_str.replace("world", "python"))
+
